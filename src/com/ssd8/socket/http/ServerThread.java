473a93897b67bfcf194bbf47b2f51a89864e24ee
zk1127 <zengkang0918@foxmail.com>
2017-11-27 15:35:24 +0800

    commit second

--- src/com/ssd8/socket/http/ServerThread.java
+++ src/com/ssd8/socket/http/ServerThread.java
@@ -1,11 +1,34 @@
 package com.ssd8.socket.http;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @Author:zengkang
  * @date:Create in 20:10 2017/11/26
  */
 public class ServerThread {
-    final static int PORT=80;
-    final static String filebase="./ServerFiles";
+    ServerSocket ss;
+    final static int PORT = 80;
+    final static String filebase = "./ServerFiles";
+    ExecutorService executorService;
+    int POOL_SIZE = 4;
 
+    public ServerThread() throws IOException {
+        ss = new ServerSocket(PORT);
+        executorService = Executors.newFixedThreadPool(Runtime.getRuntime().
+                availableProcessors() * POOL_SIZE);
+        System.out.println("服务器启动.....");
+    }
+
+    public void servic() throws IOException {
+        Socket socket = null;
+        while (true){
+            socket = ss.accept();
+            executorService.execute(new ServerHandler(socket));
+        }
+    }
 }
