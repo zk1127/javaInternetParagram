c383ffd7eeba006f72d415e6e9244409dcc0b8f8
zk1127 <zengkang0918@foxmail.com>
2017-11-27 15:09:09 +0800

    commit second

---
+++ src/com/ssd8/socket/http/ServerHandler.java
@@ -0,0 +1,16 @@
+package com.ssd8.socket.http;
+
+import java.net.Socket; /**
+ * @Author:zengkang
+ * @date:Create in 15:05 2017/11/27
+ */
+public class ServerHandler implements Runnable{
+    public ServerHandler(Socket socket) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
